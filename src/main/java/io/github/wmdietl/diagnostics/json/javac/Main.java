da4e30ec1c6851fe64aa889f280ef96f8b961d8e
Werner Dietl <wdietl@gmail.com>
2020-04-09 18:37:07 -0400

    Javac output Main program.

---
+++ src/main/java/io/github/wmdietl/diagnostics/json/javac/Main.java
@@ -0,0 +1,27 @@
+package io.github.wmdietl.diagnostics.json.javac;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import io.github.wmdietl.diagnostics.JavacDiagnosticsWrapper;
+import java.util.List;
+import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
+
+/**
+ * Wrapper around javac to output diagnostics as JSON, in a simple format directly corresponding to
+ * the javac diagnostics.
+ */
+public class Main extends JavacDiagnosticsWrapper {
+    public static void main(String[] args) {
+        new Main().run(args);
+    }
+
+    /** Serialize the diagnostics using Gson. */
+    @Override
+    protected void processDiagnostics(
+            boolean result, List<Diagnostic<? extends JavaFileObject>> diagnostics) {
+        JsonDiagnosticList diags = new JsonDiagnosticList(diagnostics);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        System.out.println(gson.toJson(diags, JsonDiagnosticList.class));
+    }
+}
