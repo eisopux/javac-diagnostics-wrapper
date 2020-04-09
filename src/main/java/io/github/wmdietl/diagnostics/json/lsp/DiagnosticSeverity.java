9a284e6f4b9a55fb231d0f02b07a8b2397efe672
Werner Dietl <wdietl@gmail.com>
2020-04-09 18:38:08 -0400

    LSP output JSON wrapper classes. Adapted from https://github.com/typetools/checker-framework/pull/3137/files from @mernst with permission.

---
+++ src/main/java/io/github/wmdietl/diagnostics/json/lsp/DiagnosticSeverity.java
@@ -0,0 +1,28 @@
+package io.github.wmdietl.diagnostics.json.lsp;
+
+/** The severity of an LSP diagnostic. */
+public enum DiagnosticSeverity {
+    /** Reports an error. */
+    ERROR(1),
+
+    /** Reports a warning. */
+    WARNING(2),
+
+    /** Reports an information. */
+    INFORMATION(3),
+
+    /** Reports a hint. */
+    HINT(4);
+
+    /** The numeric value of this DiagnosticSeverity. */
+    public final int value;
+
+    /**
+     * Create a new DiagnosticSeverity.
+     *
+     * @param value the numeric value of this DiagnosticSeverity
+     */
+    private DiagnosticSeverity(int value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/io/github/wmdietl/diagnostics/json/lsp/Position.java
@@ -0,0 +1,38 @@
+package io.github.wmdietl.diagnostics.json.lsp;
+
+/**
+ * LSP position in a text document expressed as zero-based line and zero-based character offset. A
+ * position is between two characters like an ‘insert’ cursor in a editor. Special values like for
+ * example -1 to denote the end of a line are not supported.
+ */
+public class Position {
+
+    /** Use this when you have no position information. */
+    public static final Position START = new Position(0, 0);
+
+    /** Line position in a document (zero-based). */
+    public final int line;
+
+    /**
+     * Character offset on a line in a document (zero-based). Assuming that the line is represented
+     * as a string, the `character` value represents the gap between the `character` and `character
+     * + 1`.
+     */
+    public final int character;
+
+    /**
+     * Create a new Position.
+     *
+     * @param line line position in a document (zero-based)
+     * @param character character offset on a line in a document (zero-based)
+     */
+    public Position(int line, int character) {
+        this.line = line;
+        this.character = character;
+    }
+
+    @Override
+    public String toString() {
+        return line + "," + character;
+    }
+}
