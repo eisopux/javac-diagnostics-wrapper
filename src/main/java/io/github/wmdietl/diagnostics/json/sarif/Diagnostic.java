6bea2ec3a2e0356ed3ea1ac40f0cb5db9889dddd
Omer Adeel <oadeel@uwaterloo.ca>
2024-03-25 15:53:44 -0400

    finish download and print for sarif

---
+++ src/main/java/io/github/wmdietl/diagnostics/json/sarif/Diagnostic.java
@@ -0,0 +1,57 @@
+package io.github.wmdietl.diagnostics.json.sarif;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/** Represents an LSP diagnostic, such as a compiler error or warning. */
+public class Diagnostic {
+    /** The range at which the message applies. */
+    public final Range range;
+
+    /**
+     * The diagnostic's severity. Can be omitted. If omitted it is up to the client to interpret
+     * diagnostics as error, warning, info or hint.
+     */
+    public final Integer severity;
+
+    /** The diagnostic's code, which might appear in the user interface. */
+    public final String code;
+
+    /**
+     * A human-readable string describing the source of this diagnostic, e.g. 'typescript' or 'super
+     * lint'.
+     */
+    public final String source;
+
+    /** The diagnostic's message. */
+    public final String message;
+
+    /** Additional metadata about the diagnostic. */
+    public final List<Integer> tags;
+
+    /**
+     * Create a Diagnostic using all arguments.
+     *
+     * @param range the range at which the message applies
+     * @param severity the diagnostic's severity
+     * @param code the diagnostic's code, which might appear in the user interface
+     * @param source A human-readable string describing the source of this diagnostic, e.g.
+     *     'typescript' or 'super lint'.
+     * @param message the diagnostic's message
+     * @param tags additional metadata about the diagnostic
+     */
+    public Diagnostic(
+            Range range,
+            Integer severity,
+            String code,
+            String source,
+            String message,
+            List<Integer> tags) {
+        this.range = range;
+        this.severity = severity;
+        this.code = code;
+        this.source = source;
+        this.message = message;
+        this.tags = (tags == null ? null : new ArrayList<>(tags));
+    }
+}
---
+++ src/main/java/io/github/wmdietl/diagnostics/json/sarif/Main.java
@@ -0,0 +1,103 @@
+package io.github.wmdietl.diagnostics.json.sarif;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.sql.Driver;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.tools.JavaFileObject;
+import javax.tools.Tool;
+
+import io.github.wmdietl.diagnostics.JavacDiagnosticsWrapper;
+
+/** Wrapper around javac to output diagnostics as JSON, in the Sarif format. */
+public class Main extends JavacDiagnosticsWrapper {
+    public static void main(String[] args) {
+        new Main().run(args);
+    }
+
+    /** Serialize the diagnostics using the Sarif format. */
+    @Override
+    protected void processDiagnostics(
+        boolean result, List<javax.tools.Diagnostic<? extends JavaFileObject>> diagnostics) {
+        // Mapping from unique URIs to the diagnostics for that URI
+        Map<String, List<Diagnostic>> fileDiagnostics = new HashMap<>();
+        for (javax.tools.Diagnostic<? extends JavaFileObject> d : diagnostics) {
+            JavaFileObject file = d.getSource();
+            String source = file != null ? file.toUri().toString() : "unknown file";
+            if (!fileDiagnostics.containsKey(source)) {
+                fileDiagnostics.put(source, new ArrayList<>());
+            }
+            fileDiagnostics.get(source).add(convert(d));
+        }
+
+        // Convert to FileDiagnostics for JSON identifiers
+        List<FileDiagnostics> jsonDiagnostics = new ArrayList<>();
+        for (Map.Entry<String, List<Diagnostic>> entry : fileDiagnostics.entrySet()) {
+            jsonDiagnostics.add(new FileDiagnostics(entry.getKey(), entry.getValue()));
+        }
+
+        Sarif.Rule newRule = new Sarif.Rule("Renew", "RuleRenew", "https://github.com/eisopux/javac-diagnostics-wrapper", "\"https://pvs-studio.com/en/renewal/\"");
+           
+        List<Sarif.Rule> rules = new ArrayList<>();
+        rules.add(newRule);
+        Sarif.Driver driver = new Sarif.Driver("Java Diagnostics Wrapper", "https://github.com/eisopux/javac-diagnostics-wrapper", rules);
+
+        Sarif sarifObj = new Sarif(driver);
+        sarifObj.addResults(jsonDiagnostics);
+        String results = sarifObj.toJson();
+        sarifObj.downloadAsSARIF();
+        System.out.println(results);
+    }
+
+    private Diagnostic convert(javax.tools.Diagnostic<? extends JavaFileObject> diagnostic) {
+        DiagnosticSeverity severity;
+        switch (diagnostic.getKind()) {
+            case ERROR:
+                severity = DiagnosticSeverity.ERROR;
+                break;
+            case WARNING:
+            case MANDATORY_WARNING:
+                severity = DiagnosticSeverity.WARNING;
+                break;
+            case NOTE:
+            case OTHER:
+                severity = DiagnosticSeverity.INFORMATION;
+                break;
+            default:
+                throw new IllegalArgumentException("Unexpected diagnostic kind in: " + diagnostic);
+        }
+
+        int line = (int) diagnostic.getLineNumber();
+        int column = (int) diagnostic.getColumnNumber();
+        Range range;
+        if (line < 1 || column < 1) {
+            // Use beginning of document for invalid locations
+            range = new Range(Position.START, Position.START);
+        } else {
+            // javac is 1-based whereas LSP is 0-based
+            Position start = new Position(line - 1, column - 1);
+            Position end =
+                    new Position(
+                            line - 1,
+                            (int)
+                                    (column
+                                            - 1
+                                            + diagnostic.getEndPosition()
+                                            - diagnostic.getStartPosition()));
+            range = new Range(start, end);
+        }
+
+        return new Diagnostic(
+                range,
+                severity.value,
+                diagnostic.getCode(),
+                this.getClass().getCanonicalName(),
+                diagnostic.getMessage(null),
+                null);
+    }
+}
