f06b667055aec4ffc88479d5aa395617ad67c664
Wanxuan Li <lzlu1018@gmail.com>
2024-10-30 15:45:14 -0400

    extract helper function

--- src/main/java/io/github/wmdietl/diagnostics/json/common/DiagnosticSeverity.java
+++ src/main/java/io/github/wmdietl/diagnostics/json/common/DiagnosticSeverity.java
@@ -1,5 +1,7 @@
 package io.github.wmdietl.diagnostics.json.common;
 
+import javax.tools.Diagnostic.Kind;
+
 /** The severity of an LSP diagnostic. */
 public enum DiagnosticSeverity {
     /** Reports an error. */
@@ -24,5 +26,25 @@ public enum DiagnosticSeverity {
      */
     private DiagnosticSeverity(int value) {
         this.value = value;
+    }
+
+    /**
+     * Convert from javac diagnostic Kind to DiagnosticSeverity
+     * 
+     * @param kind the severity of the diagnostic produced by javac
+     */
+    public static DiagnosticSeverity convert(final Kind kind) throws IllegalArgumentException {
+        switch (kind) {
+            case ERROR:
+                return DiagnosticSeverity.ERROR;
+            case WARNING:
+            case MANDATORY_WARNING:
+                return DiagnosticSeverity.WARNING;
+            case NOTE:
+            case OTHER:
+                return DiagnosticSeverity.INFORMATION;
+            default:
+                throw new IllegalArgumentException("Unexpected diagnostic kind");
+        }
     }
 }
