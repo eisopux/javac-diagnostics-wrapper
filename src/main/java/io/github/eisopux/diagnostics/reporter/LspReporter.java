94267937f5fd04122680b8526dc9c33560d52d01
Werner Dietl <wdietl@gmail.com>
2025-12-31 13:26:27 -0500

    Address comments

--- src/main/java/io/github/eisopux/diagnostics/reporter/LspReporter.java
+++ src/main/java/io/github/eisopux/diagnostics/reporter/LspReporter.java
@@ -162,11 +162,11 @@ public class LspReporter implements Reporter {
 
         private final int lspSeverity;
 
-        /*class-private*/ DiagnosticKind(int severity) {
+        private DiagnosticKind(int severity) {
             this.lspSeverity = severity;
         }
 
-        /*class-private*/ static int fromString(String kindStr) {
+        static int fromString(String kindStr) {
             if (kindStr == null) {
                 return INFORMATION.lspSeverity;
             }
